1326b3120ef27c4933efffa07ce545ed8944d0fc
Felipe Moura <felipewom@gmail.com>
2019-12-09 07:15:30 -0300

    First benchmark with autocannon

---
+++ src/main/java/de/nerden/samples/graal/Main.java
@@ -0,0 +1,15 @@
+package de.nerden.samples.graal;
+
+import io.javalin.Javalin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.setSomeValue("Hello World!");
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.json(t));
+    }
+}
+
+// listen and serve on 0.0.0.0:7000
---
+++ src/main/java/de/nerden/samples/graal/Test.java
@@ -0,0 +1,14 @@
+package de.nerden.samples.graal;
+
+public class Test {
+
+    private String someValue;
+
+    public String getSomeValue() {
+        return someValue;
+    }
+
+    public void setSomeValue(String someValue) {
+        this.someValue = someValue;
+    }
+}
